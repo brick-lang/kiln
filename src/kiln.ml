237eaae77a65a68b85ab3b59548643e3163757e5
Katherine Whitlock <toroidalcode@gmail.com>
2014-03-10 17:03:09 -0400

    Basic command line parsing

---
+++ src/kiln.ml
@@ -0,0 +1,23 @@
+open Core.Std
+
+let fire =
+  Command.basic ~summary:"Use kiln to compile the project"
+    Command.Spec.(
+      empty
+      +> anon ("file" %: string)
+    )
+    (fun file () -> print_string file )
+
+let glaze =
+  Command.basic ~summary:"Start the interactive toplevel"
+    Command.Spec.(
+      empty
+      (* +> option ("file" %: string) *)
+    )
+    (fun (* file *) () -> () )
+
+let command =
+  Command.group ~summary:"Manipulate dates"
+    [ "fire", fire; "glaze",  glaze]
+
+let () = Command.run command
